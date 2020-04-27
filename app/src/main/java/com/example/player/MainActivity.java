0e6f17a6536da840d752914be352bb55001cdac5
Gulshan <chandoliagulahan@gmail.com>
2020-04-27 17:24:57 +0530

    File Select Inplement

--- app/src/main/java/com/example/player/MainActivity.java
+++ app/src/main/java/com/example/player/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.player;
 
-
+    import androidx.annotation.Nullable;
     import androidx.appcompat.app.AppCompatActivity;
+
+    import android.content.Intent;
     import android.media.MediaPlayer;
     import android.os.Bundle;
     import android.view.View;
     import android.widget.EditText;
+    import android.widget.TextView;
     import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer player;
     private EditText loop;
     int n;
+    String path;
+    Intent myfiles;
+    TextView rePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         loop = (EditText) findViewById(R.id.loop);
         loop.setText("1");
+        rePath = (TextView) findViewById(R.id.filepath);
     }
 
 //    public void pause(View v) {
@@ -44,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         n = Integer.parseInt(loop.getText().toString());
 
-        player = MediaPlayer.create(this, R.raw.tone);
+        player = MediaPlayer.create(this,R.raw.tone);
         player.start();
         Toast.makeText(this, "MediaPlayer Played", Toast.LENGTH_SHORT).show();
         player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -62,9 +70,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void file_select(View v){
+
+        myfiles = new Intent(Intent.ACTION_GET_CONTENT);
+        myfiles.setType("*/*");
+        startActivityForResult(myfiles,10);
+
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
         stopPlayer();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case (10):
+                if (resultCode==RESULT_OK){
+                    path = data.getData().getPath();
+                    rePath.setText(path);
+                }
+                break;
+        }
+
     }
 }
